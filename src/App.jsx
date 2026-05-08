178a9df4af4f81729d625a1945ed5a6c18c97c68
ayanPaul082001 <ayanpaul259@gmail.com>
2026-05-08 12:10:58 +0530

    done applications

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Applyjob from "./pages/Applyjob"
-import Application from "./pages/Applications"
-import RecruiterLogin from "./components/RecruiterLogin"
-import { useContext } from "react"
-import { AppContext } from "./context/AppContext"
-import Dashboard from "./pages/Dashboard"
-import AddJob from "./pages/AddJob"
-import ManageJobs from "./pages/ManageJobs"
-import ViewApplications from "./pages/ViewApplications"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Applyjob from "./pages/Applyjob";
+import Application from "./pages/Applications";
+import RecruiterLogin from "./components/RecruiterLogin";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
+import "quill/dist/quill.snow.css";
 
 const App = () => {
-
-  const{showRecruiterLogin} = useContext(AppContext)
-// showRecruiterLogin,setShowRecruiterLogin
+  const { showRecruiterLogin } = useContext(AppContext);
 
   return (
     <div>
-    {showRecruiterLogin && <RecruiterLogin/>}
+      {showRecruiterLogin && <RecruiterLogin />}
+
       <Routes>
-        <Route path="/" element ={<Home/>} />
-         <Route path="/apply-jobs/:id" element ={<Applyjob/>} />
-          <Route path="/applications" element ={< Application />} />
-          <Route path="/dashboard" element = {<Dashboard/>}>
-            <Route  path="add-job" element = {AddJob}/>
-             <Route  path="manage-job" element = {ManageJobs}/> 
-             <Route  path="view-applications" element = {ViewApplications}/>
-          </Route>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/apply-jobs/:id" element={<Applyjob />} />
+
+        <Route path="/applications" element={<Application />} />
+
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+
+          <Route path="manage-job" element={<ManageJobs />} />
+
+          <Route
+            path="view-applications"
+            element={<ViewApplications />}
+          />
+        </Route>
       </Routes>
-      
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,35 +1,34 @@
-// import { Outlet } from "react-router-dom";
 import { assets } from "../assets/assets";
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
+
 const Dashboard = () => {
   const navigate = useNavigate();
 
   return (
     <div className="min-h-screen">
-      {/* navbar for req panale */}
+      {/* navbar for req panel */}
       <div className="shadow py-4">
         <div className="px-5 flex justify-between items-center">
           <img
-            onClick={(e) => navigate("/")}
-            className="max-sm:w-32 cursor-pointer "
+            onClick={() => navigate("/")}
+            className="max-sm:w-32 cursor-pointer"
             src={assets.logo}
           />
+
           <div className="flex items-center gap-3">
             <p className="max-sm:hidden">Welcome , GreatStack</p>
+
             <div className="relative group">
               <img
-                className=" w-8 border rounded-full"
+                className="w-8 border rounded-full"
                 src={assets.company_icon}
               />
+
               <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12">
                 <ul className="list-none m-0 p-1 bg-white rounded-xl border border-gray-200 shadow-lg text-sm min-w-[140px]">
                   <li className="px-4 py-2 cursor-pointer rounded-lg text-gray-700 hover:bg-gray-100 hover:text-black transition-all duration-200">
                     Logout
                   </li>
-
-                  {/* till done 4:01:23
-                  
-                  uptill the logo click and go to home */}
                 </ul>
               </div>
             </div>
@@ -37,25 +36,57 @@ const Dashboard = () => {
         </div>
       </div>
 
-      <div className="flelx items-start">
-        {/* left sidebar with option to add jobs manage job and view application */}
-        <div className="inline-block min-h-screen border-r-1">
+      <div className="flex items-start">
+        {/* left sidebar */}
+        <div className="inline-block min-h-screen border-r">
           <ul className="flex flex-col items-start pt-5 text-gray-800">
-            <NavLink className={({isActive}) => ` flex items-center  p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/add-job"}>
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${
+                  isActive &&
+                  "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/add-job"}
+            >
               <img className="min-w-4" src={assets.add_icon} />
               <p className="max-sm:hidden">Add Job</p>
             </NavLink>
-            <NavLink className={({isActive}) => ` flex items-center  p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/manage-jobs"}>
+
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${
+                  isActive &&
+                  "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/manage-job"}
+            >
               <img className="min-w-4" src={assets.home_icon} />
               <p className="max-sm:hidden">Manage Jobs</p>
             </NavLink>
-            <NavLink className={({isActive}) => ` flex items-center  p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${isActive && 'bg-blue-100 border-r-4 border-blue-500'}`} to={"/dashboard/view-applications"}>
-              <img className="min-w-4" src={assets.person_tick_icon} />
-              <p className="max-sm:hidden">View Applications</p>
-              {/* uptill   addjobs page will cont from it time ==> 4:9:46*/}
+
+            <NavLink
+              className={({ isActive }) =>
+                `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-200 ${
+                  isActive &&
+                  "bg-blue-100 border-r-4 border-blue-500"
+                }`
+              }
+              to={"/dashboard/view-applications"}
+            >
+              <img
+                className="min-w-4"
+                src={assets.person_tick_icon}
+              />
+
+              <p className="max-sm:hidden">
+                View Applications
+              </p>
             </NavLink>
           </ul>
         </div>
+
         <div>
           <Outlet />
         </div>
