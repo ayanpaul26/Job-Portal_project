1cc729fffba87610f1d812fedc4fe2032c7de6b5
ayanPaul082001 <ayanpaul259@gmail.com>
2026-05-11 18:30:31 +0530

    update

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
+
 const userSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    resume: { typr: string },
-    image: { type: string ,required: true },
-})
-const User = mongoose.model('User,userSchema')
+    resume: { type: String },
+    image: { type: String, required: true },
+});
 
+const User = mongoose.model('User', userSchema);
 
 export default User;
-// done till the User and will continue withcontroller function
-// to run back end cd server & then npm run server
